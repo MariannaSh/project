bdef15317bca749e69b4f64cda614d0a1ad47a38
Marianna <226659@student.uek.krakow.pl>
2024-09-21 02:26:02 +0300

    first program

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
-"use strict";
+/* Задание на урок:
 
-// const result = confirm("Are you here?")
-// console.log(result)
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-// const answer = prompt("Вам есть 18?", "18");
-// console.log(typeof(answer));
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-const answers = [];
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-answers[0] = prompt("Как ваше имя?", "");
-answers[1] = prompt("Как ваше фамилия?", "");
-answers[2] = prompt("Сколько вам лет?", "");
+Проверить, чтобы все работало без ошибок в консоли */
 
-document.write(answers);
+'use strict';
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', +''); //команда чтобы задать вопрос пользователю
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {}, //пустой объект
+    actors : {},
+    genres : [], //пустой массив
+    privat : false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?',''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?','');
 
 
-const category = "toys";
-console.log(`https://someurl.com/${category}/5`);
+personalMovieDB.movies[a] = b; //эфективнее на []
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
 
 
-console.log(4+ +"6")
-
-const isChecked = true,
-      isClose = true;
-
-console.log(isChecked && isClose)
